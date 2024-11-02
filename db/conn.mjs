ec17e9f38fde5ddc9018de336cda4fe3e05a32a5
Jacky Yang <jackyyang1670@gmail.com>
2024-11-02 10:43:51 -0700

    Updated files

--- db/conn.mjs
+++ db/conn.mjs
@@ -8,11 +8,24 @@ let conn;
 try{
   conn = await client.connect(); // Connect to MongoDB
   console.log("Connected to MongoDB");
+
 }
 catch (e){
   console.error(e);
 }
 
 const db = await conn.db("sample_training");
+
+const grades = db.collection("grades");
+
+// single field index on class_id
+grades.createIndex({class_id: 1})
+
+// single field index on learner_id
+grades.createIndex({learner_id: 1})
+
+// compound index on class_id and learner_id
+grades.createIndex({class_id: 1, learner_id: 1})
+
 
 export default db
